bf9c9fa022ab18b5bb81a8af0a21e4e362d01d7e
Тимофей <timon.from25@gmail.com>
2022-10-23 22:48:09 +0700

    Homework on variables variables 2 lesson

--- src/Main.java
+++ src/Main.java
@@ -53,6 +53,122 @@ var friend = 19;
         System.out.println("Если в компании работает"+" "
                 +employeesInTheCompany + " " +"человек, то всего" + " "
                 +workOut + " "+"часов работы может быть поделено между сотрудниками");
+byte First = 125;
+int Second = 33_80;
+short Third = 130;
+long Fourth = 3532432432444443543L;
+float Fifth = 3.6f;
+double sixth = 3.789;
+
+// Задание 2 Переменные 2
+        float a = 27.12f;
+        long b = 987678965549l;
+        double c = 2.786;
+        boolean d = false;
+        char e = 569;
+        short q = -159;
+        int w = 27897;
+        byte r = 67;
+// Задание 3
+        byte studentsAtLP = 23;
+        byte studentsAtAS = 27;
+        byte studentsAtEA = 30;
+        short Paper = 480;
+        int PapersForTheStudent = Paper / (studentsAtLP + studentsAtAS + studentsAtEA);
+        System.out.println("На каждого ученика рассчитано" + " "
+                + PapersForTheStudent + " " + "листов бумаги");
+
+  // Задание 4
+  byte productivityInTwoMinutes = 16;
+  int productivityInOneMinute = productivityInTwoMinutes / 2;
+  int productivityInTwentyMinutes = productivityInOneMinute * 20;
+        System.out.println( "За 20 минут машина произвела"+ " " + productivityInTwentyMinutes
+        + " " + "штук");
+        int productivityPerDay = productivityInOneMinute * 1440;
+        System.out.println("За сутки машина произвела"+" "+ productivityPerDay + " " + "штук");
+        int productivityInThreeDays = productivityPerDay * 3;
+        System.out.println("За трое суток машина произвела"+" "
+                + productivityInThreeDays + " " + "штук");
+        int productivityInThirtyDays = productivityPerDay * 30;
+        System.out.println("За месяц машина произвела"+" "
+                + productivityInThirtyDays + " " + "штук");
+
+// Задание 5
+        byte TotalPaint = 120;
+       byte OneClassOfWhite = 2;
+       byte OneClassOfBrown = 4;
+       int PaintsPerClass = OneClassOfWhite + OneClassOfBrown;
+       int TotalСlasses = TotalPaint / PaintsPerClass;
+       int TotalWhite = TotalСlasses * OneClassOfWhite;
+       int TotalBrown = TotalСlasses * OneClassOfBrown;
+        System.out.println("В школе, где" + " " + TotalСlasses + " "+
+                "классов, нужно" + " " +TotalWhite + " " + "банок белой краски и" + " "
+                + TotalBrown + " " + "банок коричневой краски");
+ // Задание 6
+
+ byte OneBananas = 80;
+ byte OneHundredMlOfMilk = 105;
+ byte IceCreamBriquette =  100;
+ byte Egg = 70;
+ int TotalWeight = (OneBananas * 5) + (OneHundredMlOfMilk * 2)
+         + (IceCreamBriquette * 2) + (Egg * 4);
+ short gramsPerKilogram = 1000;
+ float weightPerKilogram = TotalWeight / (float) gramsPerKilogram;
+        System.out.println( " Общий вес блюда" + " " + weightPerKilogram + " " + " килограмм");
+
+ // Задание 7
+ byte NeedToReset = 7; // кг
+ short GramsPerKilogram = 1000; // грамм
+        int NeedToResetGr = NeedToReset * GramsPerKilogram;
+ short MinLose = 250;
+ short MaxLose = 500;
+ float DaysToMinLose = NeedToResetGr / MinLose;
+ float DaysToMaxLose = NeedToResetGr / MaxLose;
+ float DaysToAveLose = (DaysToMaxLose)% DaysToMinLose;
+        System.out.println("Если спортсмен будет худеть по" +" " + MinLose + "грамм, то похудеет на"
+                +" " + NeedToReset +" " + " Кг, за" +" " + DaysToMinLose +" дней " + "Если " +
+                "спортсмен будет худеть по " + " " + MaxLose + " " + "грамм, то похудеет на"
+                +" " + NeedToReset +" " + " Кг, за" +" " + DaysToMaxLose +" "+
+                "В среднем спортсмен похудеет на" + NeedToReset + " " +  "кг за" + DaysToAveLose );
+int M = 67760;
+int D = 84690;
+int K = 76230;
+double MRaising = M * 0.1f;
+double DRaising = D * 0.1f;
+double KRaising = K * 0.1f;
+// так мы узнаем сколько будет прибавка
+        double MWillReceive = M + MRaising;
+        double DWillReceive = D + DRaising;
+        double KWillReceive = K + KRaising;
+int MAnnualIncome = M * 12;
+int DAnnualIncome = D * 12;
+int KAnnualIncome = K * 12;
+// так мы узнаем годовую зп до повышения
+        double MAnnualIncomeRaising = MWillReceive * 12;
+        double DAnnualIncomeRaising = DWillReceive * 12;
+        double KAnnualIncomeRaising = KWillReceive * 12;
+        // Так узнаем годовую зп после повышения
+        double MAnnualIncomeAfterTheIncrease = MAnnualIncomeRaising - MAnnualIncome;
+        double DAnnualIncomeAfterTheIncrease = DAnnualIncomeRaising - DAnnualIncome;
+        double KAnnualIncomeAfterTheIncrease = KAnnualIncomeRaising - KAnnualIncome;
+        // Так узнаем на сколько вырос годовой доход
+        System.out.println( "Маша теперь получает" +" " + MWillReceive +" " + "рублей. " +
+                "Годовой доход вырос на" +" " + MAnnualIncomeAfterTheIncrease +" " + "рублей");
+        System.out.println( "Денис теперь получает" +" " + DWillReceive +" " + "рублей. " +
+                "Годовой доход вырос на" +" " + DAnnualIncomeAfterTheIncrease +" " + "рублей");
+        System.out.println( "Кристина теперь получает"+" " + KWillReceive +" " + "рублей. " +
+                "Годовой доход вырос на"+" " + KAnnualIncomeAfterTheIncrease +" " + "рублей");
 
 
-}}
+
+
+
+
+
+
+
+
+
+
+
+    }}
